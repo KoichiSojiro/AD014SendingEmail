7f83d30b01eda20d324f7dfd6ff724481e16fd6e
Koichi <koichisojiro@live.com>
2016-09-16 23:51:20 +0700

    initial

---
+++ app/src/main/java/com/example/trannh08/ad014sendingemail/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.trannh08.ad014sendingemail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
